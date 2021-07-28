6aefee1b9201c51a11b5d831b81dfe8cb5389ac5
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-07-28 14:57:53 -0400

    First method created

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace NumberGuesser
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void DisplayGreeting()
+        {
+
+            Console.WriteLine("Welcome to Number Guesser");
+            Console.WriteLine("");
+            Console.WriteLine("Rules of the Game");
+            Console.WriteLine("1. Think of a number between 1 - 1024");
+            Console.WriteLine("2. I will guess the number");
+            Console.WriteLine("3. You will indicate whether my guess is Correct, Too High or Too Low");
+            Console.WriteLine("Lets begin!");
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
+        }
     }
-  }
 }
